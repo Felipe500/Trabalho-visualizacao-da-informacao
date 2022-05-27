a0d4924ec85678e9c1d9679003c687e11b2b6fda
felipe256 <felipe.oliveira@nre.tec.br>
2022-05-27 01:07:01 -0300

    ajustes e add treemap

--- plot03.py
+++ plot03.py
@@ -1,51 +1,84 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+import datetime
 import numpy as np
+import pandas as pd
+
 
 
 df = pd.read_csv('dados/HIST_PAINEL_COVIDBR_2022_Parte1_16mai2022.csv', sep=';', parse_dates=['data'])
-df.head()
-
 df['casosAcumulado'] = df['casosAcumulado'].astype(int)
 
 df['ano'] = df['data'].dt.year
 df['mes'] = df['data'].dt.month
-df.head()
-df.describe()
 
 colunas = df.keys()
-print(colunas)
+
 
 nordeste = df.query('regiao == "Nordeste" and ano == 2022')
-nordeste_mes_5 = df.query('regiao == "Nordeste" and ano == 2022 and mes==5')
-nordeste_mes_5.head()
-#veja quais os anos estão filtrados
-nordeste['ano'].unique()
 
+
+
+#substuindo Nan por 0
 nordeste = nordeste.replace(np.nan, 0)
-nordeste.head()
+
 
 
 #agupar valores de casos por estado, ano e mês.
 dt_agrupado_nordeste = nordeste.groupby(['estado','mes'])['casosNovos', 'obitosNovos'].max().reset_index()
 dt_agrupado_nordestsum = nordeste.groupby(['estado'])['casosNovos', 'obitosNovos'].sum().reset_index()
-dt_agrupado_nordeste2 = nordeste_mes_5.groupby(['estado','mes'])['casosNovos', 'obitosNovos'].max().reset_index()
-dt_agrupado_nordeste.head()
+
 
 
 itens_estados = dt_agrupado_nordeste['estado'].unique()
+
+
+
+
+
+
+# Convert data frame
+piaui = dt_agrupado_nordeste.loc[dt_agrupado_nordeste['estado']=='CE']
+
+df1=piaui['casosNovos']
+df2=piaui['mes']
+#df1.query('estado == PI')
+print(df1)
+df = pd.DataFrame({'date': np.array([datetime.datetime(2021,
+                    12, i+1) for i in range(20)]),
+                   'blogs_read': [4, 6, 5, 8, 15, 13, 18, 6, 5,
+                  3, 15, 14, 19, 21, 15, 19, 25, 24, 16, 26]})
+
+df = pd.DataFrame({'date': np.array([datetime.datetime(2021,
+                     12, i+1)
+ for i in range(20)]),
+                   'blogs_unread': [1, 1, 2, 3, 3, 3, 4, 3, 2,
+                    3, 4, 7, 5, 3, 2, 4, 3, 6, 1, 2]})
+
+itens_estados = dt_agrupado_nordeste['estado'].unique()
+labels = ['JANEIRO', 'FEVEREIRO', 'MARCO', 'ABRIL', 'MAIO']
 print(itens_estados)
+# Plot time series
+for estado in itens_estados:
+  set_estado = dt_agrupado_nordeste.loc[dt_agrupado_nordeste['estado']==estado]
+  plt.plot(set_estado['mes'], set_estado['casosNovos'], label=estado,
+          linewidth=3, marker="o",  linestyle="--")
+  #plt.plot.set_xticks(set_estado['mes'], labels)
 
+# Add title and labels
 
-from typing import Text
-#construir grafico
-meses = [1,2,3,4,5]
-titulo = 'Numero de casos ao longo dos meses no nordeste'
-fig = px.bar(dt_agrupado_nordeste2, x='mes', y='casosNovos', color='estado',barmode='group', title=titulo)
-fig.update_traces( textposition='outside')
-fig.update_xaxes(type='category')
+plt.title('NÚMERO DE CASOS DE COVID NOS ESTADOS DO NORDESTE EM 2022')
+plt.xlabel('MESES')
 
-fig.show()
+plt.ylabel('N° CASOS')
+
+# Add legend
+
+plt.legend()
+
+# Auto space
+
+plt.tight_layout()
+
+# Display plot
+
+plt.show()
